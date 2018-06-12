17e6341b78bbfd024a3b74433fa6da63a79ed5e8
John P Cavalieri <jcavalieri8619@gmail.com>
2018-06-12 11:13:58 -0400

    added width, height fields and added GSON annotations

--- app/src/main/java/com/bignerdranch/android/flickrphotogallery/GalleryItemEntity.java
+++ app/src/main/java/com/bignerdranch/android/flickrphotogallery/GalleryItemEntity.java
@@ -1,13 +1,55 @@
 package com.bignerdranch.android.flickrphotogallery;
 
 import com.bignerdranch.android.flickrphotogallery.models.GalleryItem;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
 
 public class GalleryItemEntity implements GalleryItem {
 
 
+    @Expose
+    @SerializedName("id")
     private String mID;
+
+    @Expose
+    @SerializedName("title")
     private String mCaption;
+
+    @Expose
+    @SerializedName("url_s")
     private String mUrl;
+
+    @Expose
+    @SerializedName("height_s")
+    private String mHeight;
+
+    @Expose
+    @SerializedName("width_s")
+    private String mWidth;
+
+
+
+    @Override
+    public String getHeight() {
+        return mHeight;
+    }
+
+    @Override
+    public void setHeight(final String height) {
+
+        mHeight = height;
+    }
+
+    @Override
+    public void setWidth(String width) {
+
+        mWidth = width;
+    }
+
+    @Override
+    public String getWidth() {
+        return mWidth;
+    }
 
     @Override
     public String getID() {
