fab3e79c7e4407695d2c3f098d05f4d6cc339d17
John P Cavalieri <jcavalieri8619@gmail.com>
2018-10-17 16:46:27 -0400

    added methods to retrieve owner

--- app/src/main/java/com/bignerdranch/android/flickrphotogallery/models/GalleryItem.java
+++ app/src/main/java/com/bignerdranch/android/flickrphotogallery/models/GalleryItem.java
@@ -14,6 +14,10 @@ public interface GalleryItem {
 
     void setID(String ID);
 
+    String getOwner();
+
+    void setOwner(String owner);
+
     String getCaption();
 
     void setCaption(String caption);
